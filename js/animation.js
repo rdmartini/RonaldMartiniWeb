9268f4294cb555a504dd73543008d215a8d99225
Ronald Martini <ramd1808@gmail.com>
2022-03-10 14:56:32 -0300

    portfolio web

---
+++ js/animation.js
@@ -0,0 +1,49 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.navbar', {
+    duration: 4000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+sr.reveal('.header-titulo', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-50px'
+});
+
+sr.reveal('.seccion-titulo', {
+    duration: 3000,
+    origin: 'left',
+    distance: '100px'
+});
+
+sr.reveal('.seccion-titulo2', {
+    duration: 2000,
+    origin: 'top',
+    distance: '50px'
+});
+
+sr.reveal('.card-left', {
+    duration: 2000,
+    origin: 'top',
+    distance: '70px'
+});
+
+sr.reveal('.card-bottom', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '70px'
+});
+
+sr.reveal('.card-right', {
+    duration: 2000,
+    origin: 'right',
+    distance: '70px'
+});
+
+sr.reveal('.titulo-proyect', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '50px'
+});
